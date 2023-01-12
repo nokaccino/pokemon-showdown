c30f2ef70129f3600b28d941ccf94c9bf29b18f2
nokaccino <nokaccino@outlook.com>
2023-01-11 18:40:03 -0600

    ddd
    ddd

--- data/text/customModesIkus.ts
+++
@@ -1,7 +0,0 @@
-	{
-		name: "[Gen 9+] IKUS Mod",
-		desc: `A modified multi-generation ruleset implementing various new Pok&eacute;mon, abilities, balances, and buffs.`,
-
-		mod: 'gen9',
-		ruleset: ['Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod'],
-	},
